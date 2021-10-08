f3bc3aabaeb5ef59233cb4f5645c4274f84a67df
Harshit Aditya <71604531+HarshitAditya27@users.noreply.github.com>
2021-10-08 23:26:07 +0530

    Update Reverse of a number.cpp

--- Reverse of a number.cpp
+++ Reverse of a number.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int n1 {0}; 
-  int n2 {1}; 
-  int n,nt;  
-  cout<< "Enter number"<< endl; 
-  cin>>n;   
-  cout<<"Fibonaci Series: ";
-  for (int i =1; i<=n;i++) {
-    if (i == 1) { 
-      cout<<n1<<","; 
-      continue;
-    } 
-    if (i == 2) {
-      cout<<n2<<","; 
-      continue; 
-    } 
-    nt = n1+n2; 
-    n1= n2; 
-    n2= nt; 
-    cout<<nt<<",";
-  }
+  int num,remainder; 
+  int reversenum = 0; 
+  cout<<"Enter Number: "; 
+  cin>>num; 
+  while (num!=0) {
+    remainder = num % 10;  
+    reversenum= reversenum*10 + remainder; 
+    num/=10;
+  } 
+  cout<<"Reverse of the Number: "<<reversenum; 
   return 0;
 }
