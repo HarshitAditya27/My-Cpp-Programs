f7220621de764f5ca8844bbe25d111e162a1a426
Harshit Aditya <71604531+HarshitAditya27@users.noreply.github.com>
2021-10-08 14:07:33 +0530

    Create Playing with Variablles.cpp

---
+++ Playing with Variablles.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+int main() {
+  int coin1 {100}; 
+  int coin2 {20};  
+  int coin3 {50}; 
+  string name {"Harshit"}; 
+  char gd {'A'}; 
+  cout<<"Peter has "<<coin1<<"coins"<<endl; 
+  cout<<"Tony has "<<coin2<<"coins"<<endl; 
+  cout<<"Steve has "<<coin2<<"coins"<<endl;
+  cout << "Hello "<< name<<" You are an "<<gd<<" grade person"; 
+  return 0;
+  }
