ea9a3e6d14f69d39f5d6ef19fa872a9fba04725f
Harshit Aditya <71604531+HarshitAditya27@users.noreply.github.com>
2022-06-24 23:58:05 +0530

    Create Pattern Printing 2.cpp

---
+++ Pattern Printing 2.cpp
@@ -0,0 +1,28 @@
+/*
+
+Write a program to print the following pattern:
+
+* * * * *  
+* * * * *  
+* * * * *  
+* * * * *  
+* * * * * 
+
+*/ 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= n; col++)
+        {
+            cout << "* ";
+        }
+        cout << " " << endl;
+    }
+    return 0;
+}
